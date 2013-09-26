4f84e5644875f6c4e34665e4d43ffb6800d9127e
Ryan Leckey <leckey.ryan@gmail.com>
2013-09-26 09:55:16 -0700

    Update meta.py

--- src/hocr/meta.py
+++ src/hocr/meta.py
@@ -1,2 +1,3 @@
 version = '0.2.0'
-description = 'HOCR manipulation and utility library; provides hocr2pdf binary.'
+description = ('HOCR manipulation and utility library; '
+               'provides hocr2pdf binary.')
