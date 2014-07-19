ba63c11633788b4cee5e84ab3c9a46c408c8ddb3
Ryan Leckey <rleckey@concordusapps.com>
2014-07-18 17:48:29 -0700

    Update overlay algorithm to be awesome.

--- src/hocr/overlay.py
+++ src/hocr/overlay.py
@@ -1,9 +1,102 @@
 # -*- coding: utf-8 -*-
+import itertools
 import six
 from hummus import Document, Font, Text, Image
 from .parser import parse
 from PIL import ImageFont
 import magic
+
+
+class Line:
+
+    def __init__(self):
+        self.words = []
+
+
+def _is_within(value, compare, threshold=0.005):
+    return (compare * (1 - threshold)) < value < (compare * (1 + threshold))
+
+
+def _collect_lines(words):
+    words.sort(key=lambda w: w.box.top)
+    lines = []
+    current_line = []
+    current_top = None
+    for word in words:
+        if current_top is None:
+            current_line.append(word)
+            current_top = word.box.top
+
+        elif _is_within(word.box.top, current_top):
+            current_line.append(word)
+            current_top = (current_top + word.box.top) / 2.0
+
+        else:
+            lines.append(current_line)
+            current_line = []
+            current_line.append(word)
+            current_top = word.box.top
+    if current_line:
+        lines.append(current_line)
+    return lines
+
+
+def _join_words(groups):
+    words = []
+    for group in groups:
+        group.sort(key=lambda w: w.box.left)
+        text = ""
+        top = group[0].box.top
+        left = group[0].box.left
+        for word in group:
+            text = text + " " + word.text
+            top = (top + word.box.top) / 2.0
+        top = int(round(top))
+        word = group[0]
+        word.box.top = top
+        word.box.right = group[-1].box.right
+        word.box.left = left
+        word.text = text
+        words.append(word)
+    return words
+
+
+def _align_words(groups):
+    words = []
+    for group in groups:
+        group.sort(key=lambda w: w.box.left)
+        top = group[0].box.top
+        for word in group:
+            word.box.top = top
+            words.append(word)
+
+    return words
+
+
+def _split_lines(lines):
+    chunks = []
+    for line in lines:
+        blocks = []
+        cur_block = []
+        cur_right = None
+        line.sort(key=lambda w: w.box.left)
+        for word in line:
+            if cur_right is None:
+                cur_block.append(word)
+                cur_right = word.box.right
+
+            elif _is_within(word.box.left, cur_right, threshold=0.25):
+                cur_right = word.box.right
+                cur_block.append(word)
+
+            else:
+                blocks.append(cur_block)
+                cur_right = word.box.right
+                cur_block = [word]
+        if cur_block:
+            blocks.append(cur_block)
+        chunks.append(blocks)
+    return list(itertools.chain(*chunks))
 
 
 def _is_document(source):
@@ -64,8 +157,20 @@ def overlay(output, source, text, index=0, font='TimesNewRoman', dpi=72.0):
             # Figure out scale.
             scale = ctx.box.right / page.box.right
 
+            # Filter out any words that are "empty"
+            words = list(filter(lambda w: bool(w.text.strip()), page.words))
+
+            # Collect the words into the lines of the page.
+            lines = _collect_lines(words)
+
+            # Split the lines if it does go across the whole page.
+            lines = _split_lines(lines)
+
+            # Join the list of list of boxes
+            words = _join_words(lines)
+
             # Iterate through words in the HOCR page.
-            for word in page.words:
+            for word in words:
 
                 # Skip if we don't have text.
                 text = word.text.strip()
@@ -101,4 +206,5 @@ def overlay(output, source, text, index=0, font='TimesNewRoman', dpi=72.0):
                 y -= actual_height
 
                 # Write text.
+                print(text, x, y)
                 ctx.add(Text(text, fobj, size=fsize, x=x, y=y, mode=7))
